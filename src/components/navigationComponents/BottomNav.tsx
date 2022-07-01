f3c55902b14c12b962382eb8ff25722ccca25954
DeclawedLyon <declan.e.lyons@gmail.com>
2022-07-01 16:29:38 -0700

    fixes bottom nav"

--- src/components/navigationComponents/BottomNav.tsx
+++ src/components/navigationComponents/BottomNav.tsx
@@ -4,20 +4,23 @@ import React from 'react'
 const BottomNav = () => {
   return (
     <div className="flex flex-row justify-center bottom-0 w-full ">
+      <Link as={'/'} href='/'>
+            <button className='bg-sky-500 hover:bg-sky-700 px-5 py-3 text-white rounded-lg cursor-pointer'>Home</button>
+      </Link>
       <Link as={'/horizon/declan/Map'} href={'/[company]/[user]/Map'}>
-        <button className="bg-sky-500 hover:bg-sky-700 px-5 py-3 text-white rounded-lg">Map</button>
+        <button className="bg-sky-500 hover:bg-sky-700 px-5 py-3 text-white rounded-lg cursor-pointer">Map</button>
       </Link>
       <Link as={'/horizon/declan/Schedule'} href={'/[company]/[user]/Schedule'}>
-        <button className="bg-sky-500 hover:bg-sky-700 px-5 py-3 text-white rounded-lg">Schedule</button>
+        <button className="bg-sky-500 hover:bg-sky-700 px-5 py-3 text-white rounded-lg cursor-pointer">Schedule</button>
       </Link>
       <Link as={'/horizon/Company'} href={'/[company]/Company'}>
-        <button className="bg-sky-500 hover:bg-sky-700 px-5 py-3 text-white rounded-lg">company</button>
+        <button className="bg-sky-500 hover:bg-sky-700 px-5 py-3 text-white rounded-lg cursor-pointer">company</button>
       </Link>
       <Link as={'/horizon/declan/Profile'} href={'/[company]/[user]/Profile'}>
-        <button className="bg-sky-500 hover:bg-sky-700 px-5 py-3 text-white rounded-lg">Profile Page</button>
+        <button className="bg-sky-500 hover:bg-sky-700 px-5 py-3 text-white rounded-lg cursor-pointer">Profile Page</button>
       </Link>
       <Link as={'/horizon/Scheduler'} href={'/[company]/Scheduler'}>
-        <button className="bg-sky-500 hover:bg-sky-700 px-5 py-3 text-white rounded-lg">Admin Page</button>
+        <button className="bg-sky-500 hover:bg-sky-700 px-5 py-3 text-white rounded-lg cursor-pointer">Admin Page</button>
       </Link>
     </div>
   )
